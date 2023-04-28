984c3dd9e3da357d93dd9c7ad8d80850a8482f01
LeonVerhaegh <leon.l.verhaegh@gmail.com>
2023-04-28 11:34:44 +0200

    ....

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from app import app
-from app.whitelistDoc import  create_doc_string
+#from app.whitelistDoc import  create_doc_string
 from flask import Response
 
 @app.route('/')
 def home():
-    content = create_doc_string()
+    #content = create_doc_string()
     return "Hello world"
-    return Response(content, mimetype='text/plain')
+    #return Response(content, mimetype='text/plain')
