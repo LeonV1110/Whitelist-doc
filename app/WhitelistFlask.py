fe376db777659010c6277115d529026206fb540e
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2023-04-28 18:18:45 +0200

    Update WhitelistFlask.py

--- app/WhitelistFlask.py
+++ app/WhitelistFlask.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response
 app = Flask(__name__)
-from app.whitelistDoc import  create_doc_string
+from whitelistDoc import  create_doc_string
 
 @app.route('/')
 def home():
