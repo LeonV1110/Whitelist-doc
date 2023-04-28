ff3a56e2eb3ad9505c5713de4e79ffa14c48c6ed
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2023-04-28 18:16:48 +0200

    added enviorment variables to dockerfile

--- app/WhitelistFlask.py
+++ app/WhitelistFlask.py
@@ -1,9 +1,9 @@
 from flask import Flask, Response
 app = Flask(__name__)
-#from app.whitelistDoc import  create_doc_string
+from app.whitelistDoc import  create_doc_string
 
 @app.route('/')
 def home():
-    return "hi Spectre"
-    #content = create_doc_string()
-    #return Response(content, mimetype='text/plain')
+    #return "hi Spectre"
+    content = create_doc_string()
+    return Response(content, mimetype='text/plain')
