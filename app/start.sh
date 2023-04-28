873de8703c53cd042576e9b6572359e8e32f8557
LeonVerhaegh <leon.l.verhaegh@gmail.com>
2023-04-28 11:15:05 +0200

    removed fucking whitespaces

--- app/start.sh
+++ app/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-app = "tpf:whitelistDoc"
+app="tpf:whitelistDoc"
 docker build -t ${app} .
 docker run -d -p 56733:80 \
-    --name=${app} ${app}
+    --name=${app} \
+    -v $PWD:/app ${app}
